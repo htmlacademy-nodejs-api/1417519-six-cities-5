2ed13f7620617c9bc4e894693d5118ac0498485c
Pronin Maks <maksimpronin1991@icloud.com>
2023-10-07 15:25:56 +0300

    force

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,11 +1,34 @@
-import {Logger as PinoInstance, pino} from 'pino';
+import {Logger as PinoInstance, pino, transport} from 'pino';
 import { Logger } from './logger.interface.js';
+import { getCurrentModuleDirectoryPath } from '../../helpers/file-sistem.js';
+import { resolve } from 'node:path';
+import { injectable } from 'inversify';
 
+@injectable()
 export class PinoLogger implements Logger{
   private readonly logger: PinoInstance;
 
   constructor(){
-    this.logger = pino();
+    const modulePath = getCurrentModuleDirectoryPath();
+    const logFilePath = 'logs/rest.log';
+    const destination = resolve(modulePath, '../../../', logFilePath);
+
+    const multiTransport = transport({
+      targets: [
+        {
+          target: 'pino/file',
+          options: { destination },
+          level: 'debug',
+        },
+        {
+          target:'pino/file',
+          level: 'info',
+          options:{},
+        }
+      ]});
+
+    this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created');
   }
 
   public debug(message: string, ...args: unknown[]): void {
