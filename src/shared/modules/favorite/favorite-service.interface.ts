eb19afbfb87b748bad5a504d8f5be3790ab7c1a1
Pronin Maks <maksimpronin1991@icloud.com>
2023-10-24 19:34:16 +0300

    module5-2

--- src/shared/modules/favorite/favorite-service.interface.ts
+++ src/shared/modules/favorite/favorite-service.interface.ts
@@ -1,5 +1,6 @@
-import { DocumentType } from "@typegoose/typegoose";
-import { FavoriteDto } from "./dto/favorite.dto.js";
+import { DocumentType } from '@typegoose/typegoose';
+import { FavoriteDto } from './dto/favorite.dto.js';
+import { FavoriteEntity } from './favorite.entity.js';
 
 export interface FavoriteService {
   findByUserId(userId:string): Promise<DocumentType<FavoriteEntity>[]>;
